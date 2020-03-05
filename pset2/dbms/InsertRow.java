5dde94b5240ba525ba38a55d07d13ab531387b04
Tofik Mussa <tofahub@gmail.com>
2020-03-04 19:48:41 -0500

    marshalling

--- pset2/dbms/InsertRow.java
+++ pset2/dbms/InsertRow.java
@@ -33,7 +33,7 @@ public class InsertRow {
      * Constructs an InsertRow object for a row containing the specified
      * values that is to be inserted in the specified table.
      *
-     * @param  t  the table
+     * @param  table the table
      * @param  values  the column values for the row to be inserted
      */
     public InsertRow(Table table, Object[] values) {
@@ -58,7 +58,12 @@ public class InsertRow {
      * context of our RowOutput class.)
      */
     public void marshall() throws IOException {
-        /* 
+
+        populateOffsets(offsets);
+
+        keyBuffer.writeBytes();
+
+        /*
          * PS 2: Implement this method. 
          * 
          * Feel free to also add one or more private helper methods
@@ -66,7 +71,84 @@ public class InsertRow {
          * with the appropriate offsets).
          */
     }
-        
+
+    private Column findPrimaryKey(){
+        Column primaryKeyColumn = null;
+        for(int i = 0; i < table.numColumns(); i++){
+            if(table.getColumn(i).isPrimaryKey()){
+                primaryKeyColumn = primaryKeyColumn;
+            }
+        }
+        return primaryKeyColumn;
+    }
+
+    private void populateOffsets(int[] offsets) {
+
+        int firstOffset = (offsets.length) * 2;
+        offsets[0] = populateFirstOffset(firstOffset);
+
+        for(int i = 1; i < offsets.length; i++){
+
+            Column column = table.getColumn(i);
+            int previous = i - 1;
+
+            if(!column.isNotNull()){
+                offsets[i] = IS_NULL;
+                continue;
+            }
+
+            if(column.isPrimaryKey()){
+                offsets[i] = IS_PKEY;
+                continue;
+            }
+
+            if(precededByPrimaryKeyOrNull(previous, column, i)){
+                precededByPrimaryKeyOrNull(previous, column, i);
+                continue;
+            } else {
+                if(column.getValType() == 3){
+                    offsets[i] = offsets[i - 1] + column.getValue().toString().length();
+                } else {
+                    offsets[i] = offsets[i - 1] + column.getLength();
+                }
+            }
+        }
+    }
+
+    private boolean precededByPrimaryKeyOrNull(int previous, Column column, int i) {
+        int firstOffset = (offsets.length) * 2;
+        boolean isPrecededByPKorNull = false;
+
+        while(offsets[previous] == IS_PKEY || offsets[previous] == IS_NULL){
+            if(previous == 0){
+                offsets[i] = firstOffset;
+            }
+            isPrecededByPKorNull = true;
+            previous--;
+        }
+
+        if(previous > 0 && (previous != i - 1) && (offsets[previous + 1] == IS_PKEY || offsets[previous + 1] == IS_NULL)){
+            if(column.getValType() == 3){
+                offsets[i] = offsets[previous] + column.getValue().toString().length();
+            } else {
+                offsets[i] = offsets[previous] + column.getLength();
+            }
+            isPrecededByPKorNull = true;
+        }
+        return isPrecededByPKorNull;
+    }
+
+    public int populateFirstOffset(int firstOffset){
+        if (table.getColumn(0).isPrimaryKey()) {
+            offsets[0] = IS_PKEY;
+        } else if (!table.getColumn(0).isNotNull()){
+            offsets[0] = IS_NULL;
+        } else {
+            offsets[0] = firstOffset;
+        }
+        return offsets[0];
+    }
+
     /**
      * Returns the RowOutput used for the key portion of the marshalled row.
      *
@@ -84,9 +166,9 @@ public class InsertRow {
     public RowOutput getValueBuffer() {
         return this.valueBuffer;
     }
-    
+
     /**
-     * Returns a String representation of this InsertRow object. 
+     * Returns a String representation of this InsertRow object.
      *
      * @return  a String for this InsertRow
      */
