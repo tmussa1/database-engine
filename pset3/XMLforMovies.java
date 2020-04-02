d6a7a5f237e953c9411595b077c49afdcc4e638d
Tofik Mussa <tofahub@gmail.com>
2020-04-02 18:30:58 -0400

    problem 6 completed

--- pset3/XMLforMovies.java
+++ pset3/XMLforMovies.java
@@ -52,9 +52,18 @@ public class XMLforMovies {
      * returns a string representation of that XML element
      */
     public String simpleElem(String name, String value) {
-        
-        // replace this return statement with your implementation of the method
-        return "";
+
+        StringBuilder xmlElement = new StringBuilder();
+
+        xmlElement.append("<");
+        xmlElement.append(name);
+        xmlElement.append(">");
+        xmlElement.append(value);
+        xmlElement.append("</");
+        xmlElement.append(name);
+        xmlElement.append(">");
+
+        return xmlElement.toString();
     }
     
     /*
@@ -64,9 +73,30 @@ public class XMLforMovies {
      * id number, the method returns an empty string.
      */
     public String fieldsFor(String movieID) throws SQLException {
-        
-        // replace this return statement with your implementation of the method
-        return "";
+
+        String selectColumns = "SELECT * FROM Movie WHERE id='"+ movieID+"';";
+        Statement statement = this.db.createStatement();
+        ResultSet resultSet = statement.executeQuery(selectColumns);
+        Map<String, String> resultsMap = new LinkedHashMap<>();
+
+        if(resultSet.next()){
+            for(int i = 2; i <= resultSet.getMetaData().getColumnCount(); i++) {
+                resultsMap.put(resultSet.getMetaData().getColumnName(i), resultSet.getString(i));
+            }
+        }
+
+        StringBuilder xmlResult = new StringBuilder();
+
+        for(String result : resultsMap.keySet()){
+            if(resultsMap.get(result) != null){
+                xmlResult.append("    ");
+                xmlResult.append(simpleElem(result, resultsMap.get(result)));
+                xmlResult.append("\n");
+            }
+        }
+
+
+        return xmlResult.toString().isEmpty() ? "" : xmlResult.toString();
     }
     
     /*
@@ -77,9 +107,34 @@ public class XMLforMovies {
      * id number, the method returns an empty string.
      */
     public String actorsFor(String movieID) throws SQLException {
-        
-        // replace this return statement with your implementation of the method
-        return "";
+
+        String query = "SELECT Person.name FROM Person INNER JOIN Actor on Person.id = Actor.actor_id, Movie " +
+                "WHERE Actor.movie_id = Movie.id AND Movie.id='"+movieID+"' " +
+                "ORDER BY Person.name;";
+        Statement statement = this.db.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+
+        StringBuilder innerResult = new StringBuilder();
+
+        while(resultSet.next()){
+            innerResult.append("      ");
+            innerResult.append(simpleElem("actor", resultSet.getString(1)));
+            innerResult.append("\n");
+        }
+
+        StringBuilder xmlResult = new StringBuilder();
+
+        if(innerResult.toString() != null && !innerResult.toString().isEmpty()){
+            xmlResult.append("    ");
+            xmlResult.append("<actors>");
+            xmlResult.append("\n");
+            xmlResult.append(innerResult);
+            xmlResult.append("    ");
+            xmlResult.append("</actors>");
+            xmlResult.append("\n");
+        }
+
+        return xmlResult.toString().isEmpty() ? "" : xmlResult.toString();
     }    
     
     /*
@@ -90,9 +145,34 @@ public class XMLforMovies {
      * id number, the method returns an empty string.
      */
     public String directorsFor(String movieID) throws SQLException {
-        
-        // replace this return statement with your implementation of the method
-        return "";
+
+        String query = "SELECT Person.name from Person INNER JOIN Director on Person.id=" +
+                "Director.director_id, Movie WHERE Movie.id = Director.movie_id AND Movie.id='" +
+                movieID + "' ORDER BY Person.name";
+        Statement statement = this.db.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+
+        StringBuilder innerResult = new StringBuilder();
+
+        while(resultSet.next()){
+            innerResult.append("      ");
+            innerResult.append(simpleElem("director", resultSet.getString(1)));
+            innerResult.append("\n");
+        }
+
+        StringBuilder xmlResult = new StringBuilder();
+
+        if(innerResult.toString() != null && !innerResult.toString().isEmpty()){
+            xmlResult.append("    ");
+            xmlResult.append("<directors>");
+            xmlResult.append("\n");
+            xmlResult.append(innerResult);
+            xmlResult.append("    ");
+            xmlResult.append("</directors>");
+            xmlResult.append("\n");
+        }
+
+        return xmlResult.toString().isEmpty() ? "" : xmlResult.toString();
     }    
     
     /*
@@ -103,8 +183,22 @@ public class XMLforMovies {
      * the specified id number, the method returns an empty string.
      */
     public String elementFor(String movieID) throws SQLException {
-        // replace this return statement with your implementation of the method
-        return "";
+
+        StringBuilder xmlResult = new StringBuilder();
+
+        if(!fieldsFor(movieID).isEmpty() || !actorsFor(movieID).isEmpty() || !directorsFor(movieID).isEmpty()){
+            xmlResult.append("  ");
+            xmlResult.append("<movie id=\""+movieID+"\">");
+            xmlResult.append("\n");
+            xmlResult.append(fieldsFor(movieID));
+            xmlResult.append(actorsFor(movieID));
+            xmlResult.append(directorsFor(movieID));
+            xmlResult.append("  ");
+            xmlResult.append("</movie>");
+            xmlResult.append("\n");
+        }
+
+        return xmlResult.toString().isEmpty() ? "" : xmlResult.toString();
     }
 
     /*
@@ -159,6 +253,20 @@ public class XMLforMovies {
         // Create an XMLforMovies object for the SQLite database, and
         // convert the entire database into an XML file.
         XMLforMovies xml = new XMLforMovies(dbFilename);
+//        System.out.println(xml.fieldsFor(xml.idFor("Black Panther")));
+//        System.out.println(xml.fieldsFor(xml.idFor("West Side Story")));
+//        System.out.println(xml.fieldsFor("1234567"));
+
+//        System.out.println(xml.actorsFor(xml.idFor("Black Panther")));
+//        System.out.println(xml.actorsFor(xml.idFor("Wonder Woman")));
+//        System.out.println(xml.actorsFor("1234567"));
+
+//        System.out.println(xml.directorsFor(xml.idFor("Black Panther")));
+//        System.out.println(xml.directorsFor(xml.idFor("Frozen")));
+//        System.out.println(xml.directorsFor("1234567"));
+
+        System.out.println(xml.elementFor("1234567"));
+
         xml.createFile("movies.xml");
         xml.closeDB();
     }
---
+++ pset3/ps3_queries.py
@@ -0,0 +1,61 @@
+#
+# CSCI E-66: Problem Set 3: XQuery Programming Problems
+#
+
+#
+# For each query, use a text editor to add the appropriate XQuery
+# command between the triple quotes provided for that query's variable.
+#
+# For example, here is how you would include a query that finds
+# the names of all movies in the database from 1990.
+#
+sample = """
+    for $m in //movie
+    where $m/year = 1990
+    return $m/name
+"""
+
+#
+# 1. Put your query for this problem between the triple quotes found below.
+#    Follow the same format as the model query shown above.
+#
+query1 = """
+
+"""
+
+#
+# 2. Put your query for this problem between the triple quotes found below.
+#
+query2 = """
+
+"""
+
+#
+# 3. Put your query for this problem between the triple quotes found below.
+#
+query3 = """
+
+"""
+
+#
+# 4. Put your query for this problem between the triple quotes found below.
+#
+query4 = """
+
+"""
+
+#
+# 5. Put your query for this problem between the triple quotes found below.
+#    (only required of grad-credit students)
+#
+query5 = """
+
+"""
+
+#
+# 6. Put your query for this problem between the triple quotes found below.
+#    (only required of grad-credit students)
+#
+query6 = """
+
+"""
